99d2899b2a93d446d4f5cc68c47acea622371d8f
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-11-17 13:17:28 +0100

    Add tests for #3 and #8

---
+++ test/controllers/ApplicationTest.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import static org.junit.Assert.assertEquals;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.route;
+import static play.test.Helpers.running;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import play.Logger;
+import play.mvc.Result;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by fo on 16.11.15.
+ */
+public class ApplicationTest {
+
+  @Test
+  public void testGetStatement() {
+
+    running(fakeApplication(), new Runnable() {
+      @Override
+      public void run() {
+        Result result = route(fakeRequest(routes.Application.getStatement("InC", "1.0"))
+            .header("Accept", "text/turtle"));
+        assertEquals(303, result.status());
+        assertEquals("http://null/data/InC/1.0/", result.redirectLocation());
+      }
+    });
+
+  }
+
+  @Test
+  public void testGetStatementData() {
+
+    running(fakeApplication(), new Runnable() {
+      @Override
+      public void run() {
+        Result result = route(fakeRequest(routes.Application.getStatementData("InC", "1.0", null))
+            .header("Accept", "text/turtle"));
+        assertEquals(200, result.status());
+        assertEquals("text/turtle", result.contentType());
+        assertEquals("http://null/data/InC/1.0.ttl", result.header("Content-Location"));
+        assertEquals(getResource("data/InC/1.0.ttl"), contentAsString(result));
+      }
+    });
+
+  }
+
+  private String getResource(String file) {
+    InputStream in = ClassLoader.getSystemResourceAsStream(file);
+    try {
+      return IOUtils.toString(in, "UTF-8");
+    } catch (IOException e) {
+      Logger.error(e.toString());
+      return null;
+    }
+  }
+
+}
