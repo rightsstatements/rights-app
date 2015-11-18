401f87faf370edc7afc9cd923983634d928b9646
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-11-18 18:57:04 +0100

    Add initial application controller

---
+++ app/controllers/Application.java
@@ -0,0 +1,82 @@
+package controllers;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import play.Logger;
+import play.Play;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by fo on 16.11.15.
+ */
+public class Application extends Controller {
+
+  private static Map<String, String> mimeTypeExtensionMap = new HashMap<>();
+  static {
+    mimeTypeExtensionMap.put("text/turtle", "ttl");
+    mimeTypeExtensionMap.put("application/ld+json", "jsonld");
+    mimeTypeExtensionMap.put("application/json", "jsonld");
+  }
+
+  private static Model rightsStatements = ModelFactory.createDefaultModel().read(Play.application().classloader()
+      .getResourceAsStream(Play.application().configuration().getString("rs.source.ttl")), null, "TURTLE");
+
+  private static Result getData(Model model, MimeType mimeType) {
+
+    OutputStream result = new ByteArrayOutputStream();
+
+    switch (mimeType.toString()) {
+      case "text/turtle":
+        model.write(result, "TURTLE");
+        break;
+      case "application/ld+json":
+      case "application/json":
+        model.write(result, "JSON-LD");
+        break;
+      default:
+        return status(406);
+    }
+
+    return ok(result.toString()).as(mimeType.toString());
+
+  }
+
+  private static MimeType getMimeType(Http.Request request, String ext) {
+
+    MimeType mimeType;
+
+    try {
+      if (ext != null) {
+        switch (ext) {
+          case "ttl":
+            mimeType = new MimeType("text/turtle");
+            break;
+          case "jsonld":
+            mimeType = new MimeType("application/ld+json");
+            break;
+          default:
+            mimeType = new MimeType("text/html");
+            break;
+        }
+      } else {
+        mimeType = new MimeType(request.acceptedTypes().get(0).toString());
+      }
+    } catch (MimeTypeParseException e) {
+      Logger.error(e.toString());
+      mimeType = new MimeType();
+    }
+
+    return mimeType;
+
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "rights-app"
 version := "0.1"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+libraryDependencies += "org.apache.jena" % "apache-jena-libs" % "2.13.0"
