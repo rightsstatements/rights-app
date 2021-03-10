a08c7e0bd4d5655cf4fc5ca7a62dcf73f7497780
Sascha Adler <sadler@crkn.ca>
2021-03-10 20:24:24 -0330

    updateI18n.sh now escapes single quotes as required by java.text.MessageFormat. The Italian messages file has been updated accordingly

--- updateI18n.sh
+++ updateI18n.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-tx pull -f -a
+tx pull "$@"
 
 for f in conf/messages_*.properties; do
   native2ascii $f $f
+
+  # java.text.MessageFormat uses single quotes to escape patterns that would
+  # otherwise be used for parameter substitutions. To produce a single quote,
+  # you need to escape it with itself.
+  # See https://www.playframework.com/documentation/2.4.x/ScalaI18N#Notes-on-apostrophes
+  sed -i "s/'/''/g" $f
 done
 
-git add conf/messages_*.properties
-git commit -m "Update i18n"
+echo "To commit i18n changes, use:"
+echo ""
+echo " $ git add conf/messages_*.properties"
+echo " $ git commit -m \"Update i18n\""
